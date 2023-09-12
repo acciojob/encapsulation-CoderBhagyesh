b8e56747221fc5f78ed53095e6efd88f0bceacae
Bhagyesh Khobragade <bhagyeshgk143@gmail.com>
2023-09-12 10:44:28 +0530

    Add Solution-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly ob = new RWOnly();
+        ob.name = "Bhagyesh";
+        System.out.print(ob.name);
+        // Error: 'name' has private access in 'com.driver.RWOnly'
+
+        ob.setName("Bhagyesh");
+        System.out.print(ob.getName());
+    }
 }
